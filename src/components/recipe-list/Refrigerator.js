00c368aae0fcf9db4a40e1c18bb8746b53b2be63
minseo-hong <mshong1007@gmail.com>
2023-09-08 22:01:56 +0900

    Refactor: refrigerator 컴포넌트 태그 클래스 제거

--- src/components/recipe-list/Refrigerator.js
+++ src/components/recipe-list/Refrigerator.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const RefrigeratorBlock = styled.div`
   padding: 0 1.5rem;
+`;
 
-  .content {
-    display: flex;
-    background-color: #FFF1D6;
-    border-radius: 1.5rem;
-    padding: 1.25rem 2.25rem;
-    min-height: 250px;
-  }
+const RefrigeratorCard = styled.div`
+  display: flex;
+  background-color: #FFF1D6;
+  border-radius: 1.5rem;
+  padding: 1.25rem 2.25rem;
+  min-height: 250px;
 `;
 
 const RefrigeratorGroceryBlock = styled.div`
@@ -17,17 +17,18 @@ const RefrigeratorGroceryBlock = styled.div`
   flex-direction: column;
   align-items: center;
 
-  .icon {
+  i {
     width: 40px;
     height: 40px;
     background-color: #FFFFFF;
     line-height: 40px;
     text-align: center;
     font-size: 1.25rem;
+    font-style: normal;
     border-radius: 50%;
   }
 
-  .name {
+  span {
     color: #69655F;
     font-size: 0.5rem;
     margin-top: 0.375rem;
@@ -41,8 +42,8 @@ const RefrigeratorGroceryBlock = styled.div`
 const RefrigeratorGrocery = ({ name, icon }) => {
   return (
     <RefrigeratorGroceryBlock>
-      <div className="icon">{ icon }</div>
-      <div className="name">{ name }</div>
+      <i>{ icon }</i>
+      <span>{ name }</span>
     </RefrigeratorGroceryBlock>
   );
 }
@@ -51,11 +52,11 @@ const Refrigerator = () => {
   return (
     <RefrigeratorBlock>
       <h1>나의 냉장고</h1>
-      <div className="content">
+      <RefrigeratorCard>
         <RefrigeratorGrocery name="돼지고기" icon="🥓"/>
         <RefrigeratorGrocery name="쌀" icon="🍚"/>
         <RefrigeratorGrocery name="고추" icon="🌶️"/>
-      </div>
+      </RefrigeratorCard>
     </RefrigeratorBlock>
   );
 }
