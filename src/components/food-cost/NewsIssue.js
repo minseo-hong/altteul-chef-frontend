af8e4a8030e6dbb255904a42c063eb84e26b9477
minseo-hong <mshong1007@gmail.com>
2023-09-09 11:01:34 +0900

    Refactor: NewsIssue 컴포넌트 태그 클래스 제거

--- src/components/food-cost/NewsIssue.js
+++ src/components/food-cost/NewsIssue.js
@@ -4,6 +4,8 @@ const NewsIssueBlock = styled.div`
   margin-top: 3rem;
 `;
 
+const NewsList = styled.div``;
+
 const NewsItemBlock = styled.div`
   display: flex;
   justify-content: space-between;
@@ -52,15 +54,13 @@ const NewsItem = ({ title, host, releasedDate, image }) => {
 const NewsIssue = () => {
   return (
     <NewsIssueBlock>
-      <div className="title-group">
-        <h1 className="title">오늘의 경제 이슈 살펴보기</h1>
-      </div>
-      <div>
+      <h1>오늘의 경제 이슈 살펴보기</h1>
+      <NewsList>
         <NewsItem title="쏟아지는 은행채에 주담대 또 오르나" host="해럴드 경제" 
           releasedDate="1시간 전" image="쏟아지는 은행채에 주담대 또 오르나.jpg" />
         <NewsItem title="오는 24일부터 전세사기 피해자에 싼 금리로 대환대출" host="글로벌경제신문" 
           releasedDate="2시간 전" image="오는 24일부터 전세사기 피해자에 싼 금리로 대환대출.jpg" />
-      </div>
+      </NewsList>
     </NewsIssueBlock>
   );
 }
